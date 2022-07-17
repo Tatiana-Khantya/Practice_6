3e5d6cf53523a78a74c891e701560031a7cf0e4f
Tatiana <tatiana_khantya@mail.ru>
2022-07-17 22:40:43 +0300

    Add Tasks 47, 50, 52

---
+++ Task_50/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет 
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} | ");
+            else Console.Write($"{arr[i, j],3} ]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElem(int[,] arr, int a, int b)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == a && j == b)
+            {
+                Console.WriteLine($"Значение элемента: {arr[i, j]}");
+                return;
+            }
+        }
+
+    }
+    Console.WriteLine("Такого элемента в массиве нет");
+}
+int[,] arrayResult = CreateMatrixRndInt(3, 4, 1, 20);
+PrintMatrix(arrayResult);
+Console.WriteLine("Введите позицию элемента в двумерном массиве: ");
+Console.Write("Номер строки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Номер столбца: ");
+int y = Convert.ToInt32(Console.ReadLine());
+FindElem(arrayResult, x, y);
---
+++ Task_52/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} | ");
+            else Console.Write($"{arr[i, j],3} ]");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] AverageColumn(int[,] arr)
+{
+    double[] array = new double[arr.GetLength(1)];
+    int index = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = sum + arr[i, j];
+            count++;
+        }
+        // double res = sum / count;
+        array[index] = sum / count;
+        index++;
+    }
+    return array;
+}
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(Math.Round(arr[i], 1) + "; ");
+        else Console.Write(Math.Round(arr[i], 1) + "]");
+    }
+}
+
+int[,] arrayResult = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(arrayResult);
+Console.WriteLine();
+double[] average = AverageColumn(arrayResult);
+PrintArray(average);
